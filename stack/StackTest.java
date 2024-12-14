4bf4e300d5e939f962792ce39218ca636c29edb0
ko-uema2 <uematsuku@gmail.com>
2024-12-14 12:03:05 +0900

    1-3-3. スタックが空の場合にpeekメソッドを呼び出すと例外がスローされることを確認するテスト

--- stack/StackTest.java
+++ stack/StackTest.java
@@ -3,6 +3,8 @@ package stack;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class StackTest {
@@ -28,5 +30,18 @@ public class StackTest {
         Stack stack = new Stack();
         stack.push(1);
         assertEquals(1, stack.peek());
+    }
+
+    @Test
+    public void testPeekThrowsExceptionWhenStackIsEmpty() {
+        Stack stack = new Stack();
+
+        try {
+            stack.peek();
+            fail("peek() should throw an exception when the stack is empty");
+        } catch (Exception e) {
+            assertTrue(e instanceof StackEmptyException);
+            assertEquals("Stack is empty", e.getMessage());
+        }
     }
 }
