5decea21ab71828828c1e79002973aed0ca8c426
ko-uema2 <uematsuku@gmail.com>
2024-12-15 18:18:35 +0900

    3-3-2. テストコードの修正

--- stack/StackTest.java
+++ stack/StackTest.java
@@ -11,7 +11,7 @@ public class StackTest {
 
     @Test
     public void testIsEmpty() {
-        Stack stack = new Stack();
+        Stack stack = Stack.create();
         assertTrue(stack.isEmpty());
 
         stack.push(1);
@@ -20,21 +20,21 @@ public class StackTest {
 
     @Test
     public void testPushAndPeek() {
-        Stack stack = new Stack();
+        Stack stack = Stack.create();
         stack.push(1);
         assertEquals(1, stack.peek());
     }
 
     @Test
     public void testPushAndPop() {
-        Stack stack = new Stack();
+        Stack stack = Stack.create();
         stack.push(1);
         assertEquals(1, stack.pop());
     }
 
     @Test
     public void testPopThrowsExceptionWhenStackIsEmpty() {
-        Stack stack = new Stack();
+        Stack stack = Stack.create();
 
         try {
             stack.pop();
@@ -47,7 +47,7 @@ public class StackTest {
 
     @Test
     public void testPeekThrowsExceptionWhenStackIsEmpty() {
-        Stack stack = new Stack();
+        Stack stack = Stack.create();
 
         try {
             stack.peek();
@@ -60,7 +60,7 @@ public class StackTest {
 
     @Test
     public void testPushMultipleElementsAndPopInOrder() {
-        Stack stack = new Stack();
+        Stack stack = Stack.create();
         stack.push(1);
         stack.push(2);
         stack.push(3);
@@ -73,7 +73,7 @@ public class StackTest {
 
     @Test
     public void testSize() {
-        Stack stack = new Stack();
+        Stack stack = Stack.create();
         assertEquals(0, stack.size());
 
         stack.push(1);
